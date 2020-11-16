ae2c933ef3d0b02e2cabe911a7a85c09a2bd34f1
Antoine GÉLIN <gelinantoine@gmail.com>
2020-11-16 14:54:47 +0100

    update for horizontal win detection (buggy)

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,3 +1,4 @@
+import scala.util.control.Breaks.break
 
 object Game extends App {
 
@@ -13,8 +14,73 @@ object Game extends App {
   }
 
   def win(board:Array[Array[Int]], x:Int, y:Int): Boolean ={
-    false
+    val player = 1
+    var stopX = false
+    var stopY = false
+    var nbAlignes = 0
+    var newX = x
+    var newY = y
+    var sensX = 1
+    var sensY = 1
+
+
+    // On va checker les verticaux
+    while((nbAlignes <4) && (stopX ==false)){
+      if (newX < 6 && newX >= 0) {
+        if (board(newX)(newY) == player) {
+          nbAlignes += 1
+          newX += sensX
+          println("x="+newY+" y="+newX+" nb="+nbAlignes)
+        }
+        else {
+          if (sensX == 1) {
+            newX = x - 1
+            newY = y
+            sensX = -1
+          }
+          else stopX = true
+        }
+
+      }
+      else stopX  = true
+    }
+
+
+    // On ne part dans ce check que si on a pas déjà gagné
+    if (stopX && nbAlignes < 4) {
+      nbAlignes = 0
+      newX = x
+      newY = y
+
+
+      // On va checker les horizontaux
+      while ((nbAlignes < 4) && (stopY == false)) {
+        if (newY < 6 && newY >= 0) {
+          if (board(newX)(newY) == player) {
+            nbAlignes += 1
+            newY += sensY
+            println("x=" + newY + " y=" + newX + " nb=" + nbAlignes+" (h)")
+          }
+          else {
+            if (sensY == 1) {
+              newY = y - 1
+              newX = x
+              sensX = -1
+            }
+            else stopY = true
+          }
+
+        }
+        else stopY = true
+
+
+      }
+    }
+
+    nbAlignes >= 4
   }
+
+
 
   def determineDrop(board:Array[Array[Int]], positionToDrop:Int): Any={
     if(board(5)(positionToDrop-1) == 0) 5
@@ -45,9 +111,10 @@ object Game extends App {
         val col = scala.io.StdIn.readInt()
         println()
         height = determineDrop(board, col)
-        if (height != null)
+        if (height != null) {
           board(height.asInstanceOf[Int])(col-1) = 1
-        else println("Mouvement impossible")
+          println(win(board, height.asInstanceOf[Int], col-1))
+        } else println("Mouvement impossible")
       }
     }
     winner
